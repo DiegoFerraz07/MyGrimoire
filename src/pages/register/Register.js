3571abb5570bd7ae33f38c3f1ecee28b7a2e5634
randler.ferraz <randler.ferraz@codificar.com.br>
2021-08-30 21:18:54 -0300

    correção register

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,151 +1,114 @@
 //import liraries
-import React from 'react';
+import React, {Component, useEffect, useState} from 'react';
 
 import CardForm from '../../components/cards/CardForm';
 import FormRow from '../../components/forms/FormRow';
 
-import { connect } from 'react-redux';
-import { tryRegister } from '../../actions';
-import { 
-    ImageHeader, 
-    ImageBackground, 
-    CardLogin, 
-    InputLogin, 
-    ContainerLogin, 
-    ContainerError, 
-    LoadingButton, 
-    ButtonLogin, 
-    ButtonLoginText, 
-    ErrorText,
-    ContainerApp
+import {
+  ImageHeader,
+  ImageBackground,
+  CardLogin,
+  InputLogin,
+  ContainerLogin,
+  ContainerError,
+  LoadingButton,
+  ButtonLogin,
+  ButtonLoginText,
+  ErrorText,
+  ContainerApp,
 } from './styles';
 
 const bookBackground = require('../../../resources/img/book.jpg');
 
 // create a component
-class Login extends React.Component {
-    static navigationOptions = {
-        headerRight: ( <ImageHeader source = {require('../../../resources/img/5ered.png')} /> ),
-    }
-    constructor(props) {
-        super(props);
+function Register() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
-        this.state = {
-            nome: '',
-            mail: '',
-            password: '',
-            isLoading: false,
-            message: ''
-        }
+  function tryRegister() {
+    setIsLoading(true);
+    setMessage('');
 
-    }
-
-    onChangeHandler(field, value) {
-        this.setState({
-            [field]: value
-        });
-    }
-    tryRegister() {
-        this.setState({isLoading: true, message: ''});
-        const { mail: email, password } = this.state;
-
-        if(!email || !password) {
-            this.setState({
-                message: 'Digite os dados',
-                isLoading: false
-            });
-        } else {
-            
-            this.props.tryRegister({email, password})
-            .then((user) => {
-                if (user)
-                  return this.props.navigation.replace('home');  
+    if (!email || !password) {
+      setMessage('Preencha todos os campos');
+      setIsLoading(false);
+    } else {
+      console.log('fazer o cadastro do sujeito');
+      // TODO: fazer o registro
+      /*this.props
+              .tryRegister({email, password})
+              .then(user => {
+                if (user) {
+                  return this.props.navigation.replace('home');
+                }
 
                 this.setState({
-                    isLoading: false,
-                    message: ''
+                  isLoading: false,
+                  message: '',
                 });
-            })
-            .catch( error =>{
+              })
+              .catch(error => {
                 this.setState({
-                    isLoading: false, 
-                    message: this.getMessageByErrorCode(error)
+                  isLoading: false,
+                  message: this.getMessageByErrorCode(error),
                 });
-            });
-        }
+              });*/
+    }
+  }
+
+  const renderButton = () => {
+    if (isLoading) {
+      return <LoadingButton color="#FFF" />;
+    }
+    return (
+      <ButtonLogin onPress={() => tryRegister()} underlayColor="#a37c00">
+        <ButtonLoginText>Cadastrar</ButtonLoginText>
+      </ButtonLogin>
+    );
+  };
+
+  const renderMessage = () => {
+    if (!message) {
+      return null;
     }
 
-    getMessageByErrorCode(errorCode) {
-        switch(errorCode) {
-            case 'auth/email-already-in-use':
-                return 'E-mail já está em uso';
-            case 'auth/invalid-email':
-                return 'E-mail inválido';
-            case 'auth/operation-not-allowed':
-                return 'Operação não permitida';
-            case 'auth/weak-password':
-                return 'Senha fraca';
-            default:
-                return errorCode;
-        }
-    }
+    return (
+      <ContainerError>
+        <ErrorText>{message}</ErrorText>
+      </ContainerError>
+    );
+  };
 
-    renderButton() {
-        if (this.state.isLoading)
-            return <LoadingButton color = "#FFF" />
-        return (
-            <ButtonLogin
-                onPress = { () => this.tryRegister() }
-                underlayColor = '#a37c00'>
-                <ButtonLoginText>Cadastrar</ButtonLoginText>
-            </ButtonLogin>
-        )
-    }
-    renderMessage() {
-        const { message } = this.state;
-
-        if(!message)
-            return null;
-
-        return (
-            <ContainerError>
-                <ErrorText>{ message }</ErrorText>
-            </ContainerError>
-        );
-    }
-
-    render() {
-        return (
-            <ContainerApp>
-            <ImageBackground source={ bookBackground } />
-                <CardLogin>
-                    <CardForm imagem = 'register' >
-                        <FormRow >
-                            <InputLogin
-                                placeholder = "user@mail.com"
-                                value = {this.state.mail}
-                                onChangeText = { value => this.onChangeHandler('mail', value)} />
-                        </FormRow>
-                        <FormRow last >
-                            <InputLogin
-                                placeholder = "********"
-                                value = {this.state.password}
-                                secureTextEntry
-                                onChangeText = { value => this.onChangeHandler('password', value)} 
-                            />
-                        </FormRow>
-                        { this.renderMessage() }
-                        { this.renderButton() }
-                    </CardForm>
-                </CardLogin>
-            </ContainerApp>
-        );
-    }
-};
-
-const mapDispatchToProps = {
-    tryRegister
+  return (
+    <ContainerApp>
+      <ImageBackground source={bookBackground} />
+      <CardLogin>
+        <CardForm imagem="register">
+          <FormRow>
+            <InputLogin
+              placeholder="user@mail.com"
+              value={email}
+              onChangeText={mail => setEmail(mail)}
+            />
+          </FormRow>
+          <FormRow last>
+            <InputLogin
+              placeholder="********"
+              value={password}
+              secureTextEntry
+              onChangeText={pass => setPassword(pass)}
+            />
+          </FormRow>
+          {renderMessage}
+          {renderButton}
+        </CardForm>
+      </CardLogin>
+    </ContainerApp>
+  );
 }
 
 //make this component available to the app
-export default connect(null, mapDispatchToProps)(Login);
+export default Register;
