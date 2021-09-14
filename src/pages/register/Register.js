8d656a2e283ab97c7fcb7b37fa300d7bc46ad794
randler.ferraz <randler.ferraz@codificar.com.br>
2021-09-14 08:17:14 -0300

    register funcionando

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -4,6 +4,10 @@ import React, {Component, useEffect, useState} from 'react';
 import CardForm from '../../components/cards/CardForm';
 import FormRow from '../../components/forms/FormRow';
 
+import axios from 'axios';
+
+import uuid from 'react-native-uuid';
+
 import {
   ImageHeader,
   ImageBackground,
@@ -22,13 +26,14 @@ const bookBackground = require('../../../resources/img/book.jpg');
 
 // create a component
 function Register() {
-  const [nome, setNome] = useState('');
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState('');
 
-  function tryRegister() {
+  async function tryRegister() {
     setIsLoading(true);
     setMessage('');
 
@@ -36,8 +41,33 @@ function Register() {
       setMessage('Preencha todos os campos');
       setIsLoading(false);
     } else {
-      console.log('fazer o cadastro do sujeito');
-      // TODO: fazer o registro
+      //salvar o usuário no banco de dados
+      const data = {
+        id: uuid.v4(),
+        name,
+        email,
+        password,
+      };
+
+      await axios
+        .post('http://10.0.2.2:3000/users', data)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+      setIsLoading(false);
+      // criar objeto e salvar via post pelo json-server do arquivo db
+
+      /*{
+        "id": "ApZqh2lRENc43CpU3O9S8qthq4R2",
+        "nome": "Diego",
+        "email": "dihferraz-01@hotmail.com",
+        "senha": "123456"
+      },*/
+
       /*this.props
               .tryRegister({email, password})
               .then(user => {
@@ -89,21 +119,31 @@ function Register() {
         <CardForm imagem="register">
           <FormRow>
             <InputLogin
-              placeholder="user@mail.com"
+              placeholderTextColor="#A3A3A3"
+              placeholder="Lancelot"
+              value={name}
+              onChangeText={name => setName(name)}
+            />
+          </FormRow>
+          <FormRow>
+            <InputLogin
+              placeholderTextColor="#A3A3A3"
+              placeholder="cavaleiro@tavola.com"
               value={email}
               onChangeText={mail => setEmail(mail)}
             />
           </FormRow>
           <FormRow last>
             <InputLogin
+              placeholderTextColor="#A3A3A3"
               placeholder="********"
               value={password}
               secureTextEntry
               onChangeText={pass => setPassword(pass)}
             />
           </FormRow>
-          {renderMessage}
-          {renderButton}
+          {renderMessage()}
+          {renderButton()}
         </CardForm>
       </CardLogin>
     </ContainerApp>
