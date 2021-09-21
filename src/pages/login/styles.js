5918b93a3375f7be20c883fa095b202a3f010e3f
randler.ferraz <randler.ferraz@codificar.com.br>
2021-09-21 20:14:40 -0300

    correção style

--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -7,27 +7,27 @@ export const ContainerApp = styled.View`
   padding-top: ${props => (props.nothing ? 10 : 0)};
   padding-bottom: ${props => (props.nothing ? 10 : 0)};
   padding-left: ${props => (props.nothing ? 10 : 0)};
-  padding-right: ${props => (props.nothing ? 10 : 0)};
+  padding-right: ${props => (props.nothing ? 10 : 0)}px;
 `;
 
 export const CardLogin = styled.View`
-  margin-top: 80;
+  margin-top: 80px;
 `;
 
 export const InputLogin = styled.TextInput`
-  padding-bottom: 5;
-  padding-right: 5;
-  padding-top: 5;
-  padding-left: 15;
+  padding-bottom: 5px;
+  padding-right: 5px;
+  padding-top: 5px;
+  padding-left: 15px;
   color: #fff;
-  border-bottom-width: 1;
+  border-bottom-width: 1px;
   border-bottom-color: #5b4500;
 `;
 
 export const ImageHeader = styled.Image`
-  width: 30;
-  height: ${props => (props.logout ? 24 : 30)};
-  margin-right: 20;
+  width: 30px;
+  height: ${props => (props.logout ? '24px' : '30px')};
+  margin-right: 20px;
   margin-top: ${props => (props.logout ? '10px' : 0)};
   aspect-ratio: 1;
 `;
@@ -35,33 +35,33 @@ export const TextLogoutHeader = styled.Text`
   font-size: 8px;
   font-weight: bold;
   align-self: center;
-  padding-right: 20;
-  margin-top: 3;
+  padding-right: 20px;
+  margin-top: 3px;
   color: #fff;
 `;
 
 export const ContainerLoading = styled.View`
-  height: ${Dimensions.get('window').height - 100};
+  height: ${Dimensions.get('window').height - 100}px;
   justify-content: center;
   align-items: center;
 `;
 
 export const ContainerLoadingNothing = styled.View`
-  padding-top: 10;
-  padding-bottom: 10;
-  padding-left: 10;
-  padding-right: 10;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
 
 export const ImageNothing = styled.Image`
   align-self: center;
-  margin-top: 30;
-  width: 200;
-  height: 186;
+  margin-top: 30px;
+  width: 200px;
+  height: 186px;
 `;
 
 export const TextNothing = styled.Text`
-  padding-top: 20;
+  padding-top: 20px;
   font-size: 20px;
   color: #d6a200;
   font-weight: bold;
@@ -85,28 +85,28 @@ export const TextFooterLogin = styled.Text`
 
 export const LoadingButton = styled.ActivityIndicator`
   background-color: #d6a200;
-  border-width: 1;
+  border-width: 1px;
   border-color: #5b4500;
-  margin-horizontal: 50;
-  margin-top: 10;
-  padding-top: 10;
-  padding-bottom: 10;
-  padding-left: 10;
-  padding-right: 10;
-  border-radius: 5;
+  margin-horizontal: 50px;
+  margin-top: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 5px;
 `;
 
 export const ButtonLogin = styled.TouchableOpacity`
   background-color: #d6a200;
-  border-width: 1;
+  border-width: 1px;
   border-color: #5b4500;
-  margin-horizontal: 50;
-  margin-top: 10;
-  padding-top: 10;
-  padding-bottom: 10;
-  padding-left: 10;
-  padding-right: 10;
-  border-radius: 5;
+  margin-horizontal: 50px;
+  margin-top: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 5px;
 `;
 
 export const ButtonLoginText = styled.Text`
@@ -117,7 +117,7 @@ export const ButtonLoginText = styled.Text`
 `;
 
 export const ContainerError = styled.View`
-  margin-vertical: 5;
+  margin-vertical: 5px;
 `;
 
 export const ErrorText = styled.Text`
--- src/pages/register/styles.js
+++ src/pages/register/styles.js
@@ -7,62 +7,62 @@ export const ContainerApp = styled.View`
   padding-top: ${props => (props.nothing ? 10 : 0)};
   padding-bottom: ${props => (props.nothing ? 10 : 0)};
   padding-left: ${props => (props.nothing ? 10 : 0)};
-  padding-right: ${props => (props.nothing ? 10 : 0)};
+  padding-right: ${props => (props.nothing ? 10 : 0)}px;
 `;
 
 export const CardLogin = styled.View`
-  margin-top: 80;
+  margin-top: 80px;
 `;
 
 export const InputLogin = styled.TextInput`
-  padding-bottom: 5;
-  padding-right: 5;
-  padding-top: 5;
-  padding-left: 15;
+  padding-bottom: 5px;
+  padding-right: 5px;
+  padding-top: 5px;
+  padding-left: 15px;
   color: #fff;
-  border-bottom-width: 1;
+  border-bottom-width: 1px;
   border-bottom-color: #5b4500;
 `;
 
 export const ImageHeader = styled.Image`
-  width: 30;
-  height: ${props => (props.logout ? 24 : 30)};
-  margin-right: 20;
+  width: 30px;
+  height: ${props => (props.logout ? '24px' : '30px')};
+  margin-right: 20px;
   margin-top: ${props => (props.logout ? '10px' : 0)};
   aspect-ratio: 1;
 `;
 export const TextLogoutHeader = styled.Text`
-  font-size: 8;
+  font-size: 8px;
   font-weight: bold;
   align-self: center;
-  padding-right: 20;
-  margin-top: 3;
+  padding-right: 20px;
+  margin-top: 3px;
   color: #fff;
 `;
 
 export const ContainerLoading = styled.View`
-  height: ${Dimensions.get('window').height - 100};
+  height: ${Dimensions.get('window').height - 100}px;
   justify-content: center;
   align-items: center;
 `;
 
 export const ContainerLoadingNothing = styled.View`
-  padding-top: 10;
-  padding-bottom: 10;
-  padding-left: 10;
-  padding-right: 10;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
 
 export const ImageNothing = styled.Image`
   align-self: center;
-  margin-top: 30;
-  width: 200;
-  height: 186;
+  margin-top: 30px;
+  width: 200px;
+  height: 186px;
 `;
 
 export const TextNothing = styled.Text`
-  padding-top: 20;
-  font-size: 20;
+  padding-top: 20px;
+  font-size: 20px;
   color: #d6a200;
   font-weight: bold;
   align-self: center;
@@ -77,7 +77,7 @@ export const ImageBackground = styled.Image`
 `;
 
 export const TextFooterLogin = styled.Text`
-  font-size: 16;
+  font-size: 16px;
   font-weight: bold;
   color: #d6a200;
   align-self: center;
@@ -85,44 +85,44 @@ export const TextFooterLogin = styled.Text`
 
 export const LoadingButton = styled.ActivityIndicator`
   background-color: #d6a200;
-  border-width: 1;
+  border-width: 1px;
   border-color: #5b4500;
-  margin-horizontal: 50;
-  margin-top: 10;
-  padding-top: 10;
-  padding-bottom: 10;
-  paddingleft: 10;
-  padding-right: 10;
-  border-radius: 5;
+  margin-horizontal: 50px;
+  margin-top: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 5px;
 `;
 
 export const ButtonLogin = styled.TouchableOpacity`
   background-color: #d6a200;
-  border-width: 1;
+  border-width: 1px;
   border-color: #5b4500;
-  margin-horizontal: 50;
-  margin-top: 10;
-  padding-top: 10;
-  padding-bottom: 10;
-  padding-left: 10;
-  padding-right: 10;
-  border-radius: 5;
+  margin-horizontal: 50px;
+  margin-top: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 5px;
 `;
 
 export const ButtonLoginText = styled.Text`
   color: #fff;
   font-weight: bold;
-  font-size: 18;
+  font-size: 18px;
   align-self: center;
 `;
 
 export const ContainerError = styled.View`
-  margin-vertical: 5;
+  margin-vertical: 5px;
 `;
 
 export const ErrorText = styled.Text`
   align-self: center;
   color: #fff;
-  font-size: 18;
+  font-size: 18px;
   font-weight: bold;
 `;
